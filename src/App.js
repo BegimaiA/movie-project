7e93e6a0cca1dae2867e5669c2d4661f913ea8d2
Begimai Agybaeva <begimai.a@yandex.ru>
2021-09-08 16:07:02 +0600

    added media

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Search from "./views/SearchResult"
 import Footer from "./components/Footer";
 import "./index.scss"
+import "./media.scss"
 
 
 function App() {
