964ed96df3146b305e1905bc531d6bcc547019c1
Begimai Agybaeva <begimai.a@yandex.ru>
2021-09-09 12:22:15 +0600

    deleted node

--- src/components/Trailers/index.js
+++ src/components/Trailers/index.js
@@ -8,10 +8,9 @@ const Trailers = ({id}) => {
     return (
         <div>
             <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={id} onClose={() => setOpen(false)} />
-            <button className="btn-primary" onClick={()=> setOpen(true)}>VIEW DEMO</button>
+            <button className="btn-primary video-btn" onClick={()=> setOpen(true)}>VIEW DEMO</button>
         </div>
 
-    )
-};
+    )};
 
 export default Trailers;
